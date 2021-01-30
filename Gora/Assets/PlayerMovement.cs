9cedd093c04ab52d063d0bd236427cb05ae15013
Mücahit Eren Özkur <mucahiterenozkur@hotmail.com.tr>
2021-01-30 17:26:57 +0300

    initial commit

---
+++ Gora/Assets/PlayerMovement.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float movementSpeed = 5.0f;
+
+    private Rigidbody rb;
+    private float jumpForce = 2f;
+    Vector3 jump;
+
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        jump = new Vector3(0f, jumpForce, 0f);
+    }
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.position += transform.forward * movementSpeed * Time.deltaTime;
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            transform.position -= transform.forward * movementSpeed * Time.deltaTime;
+        }
+        else if (Input.GetKey(KeyCode.A))
+        {
+            transform.position -= transform.right * movementSpeed * Time.deltaTime;
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            transform.position += transform.right * movementSpeed * Time.deltaTime;
+        }
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.AddForce(jump * jumpForce, ForceMode.Impulse);
+        }
+    }
+}
